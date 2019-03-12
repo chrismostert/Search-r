8e33d510bb24bbffd02c20d917be143baccf624b
MarkHaakman <m.p.a.haakman@student.tudelft.nl>
2019-03-12 11:31:13 +0100

    Add Mann–Whitney U test when data does not have a normal distribution

--- stats.py
+++ stats.py
@@ -2,8 +2,8 @@ import csv
 import numpy as np
 import scipy.stats
 
-f1 = input('Enter filename for file 1: ')
-f2 = input('Enter filename for file 2: ')
+f1 = 'nopressure.csv'
+f2 = 'pressure.csv'
 
 with open(f1) as csv_file:
     reader = csv.reader(csv_file)
@@ -22,8 +22,20 @@ parseInput(r2)
 
 
 for key in r1.keys():
-    print(f1, key, np.mean(r1[key]))
-    print('std',np.std(r1[key]))
-    print(f2, key, np.mean(r2[key]))
-    print('std',np.std(r2[key]))
-    print(scipy.stats.ttest_ind(r1[key], r2[key], equal_var=False))
+    r1_normal_distribution = scipy.stats.shapiro(r1[key])[1] > 0.05
+    r2_normal_distribution = scipy.stats.shapiro(r2[key])[1] > 0.05
+    if r1_normal_distribution and r2_normal_distribution:
+        print('Results have a normal distribution. t-test')
+        print(f1, key, 'mean=', np.mean(r1[key]))
+        print('std', np.std(r1[key]))
+        print(f2, key, 'mean=', np.mean(r2[key]))
+        print('std', np.std(r2[key]))
+        print(scipy.stats.ttest_ind(r1[key], r2[key], equal_var=False))
+    else:
+        print('Results do not have a normal distribution. Mann–Whitney U test.')
+        print(f1, key, 'median=', np.median(r1[key]))
+        print('se', np.std(r1[key])/np.sqrt(len(r1)))
+        print(f2, key, 'median=', np.median(r2[key]))
+        print('se', np.std(r2[key])/np.sqrt(len(r2)))
+        print(scipy.stats.mannwhitneyu(r1[key], r2[key]))
+    print()
